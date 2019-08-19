0089be2990459ecbf6e6f2db13b0d0220e9bc796
Rod Morgado <rodrigo@zooft.company>
2019-08-19 17:09:33 -0400

    Adding some comments to explain the code.

--- js/script.js
+++ js/script.js
@@ -6,23 +6,22 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/* Start with Global variables
+studentList has all the li elements that corresponds to each student
+itemsToBeDisplayed is the number of studentes to be displayed by page*/
 let studentList = document.querySelector('.student-list').children;
 const itemsToBeDisplayed = 10;
 
+/* This function displays the li elements according to the range the li elements 
+belongs to. This range is calculated using the number of the page the user is visiting.
+*/
 const displayPage = (list, pageNumber) => {
    for(i = 0; i < list.length; i++){
+      /* All the li elements are hidden. */
       list[i].style.display = 'none';
    };
+   /* In this part, the range is created and it shows only the li elements
+   that belongs to the range*/
    let startIndex = (pageNumber * itemsToBeDisplayed) - itemsToBeDisplayed;
    let endIndex = pageNumber * itemsToBeDisplayed;
    for(let i = 0; i < list.length; i++){
@@ -32,14 +31,18 @@ const displayPage = (list, pageNumber) => {
    } 
 };
 
+/* This function creates the dynamic HTML to display the pagination at the bottom 
+of the page. A list of li elements is required as parameter.*/
 const createPaginationLinks = (list) => {
+  /* HTML elements creation */
   let divPage = document.querySelector('.page');
   let divPagination = document.createElement('div');
   divPagination.className = 'pagination';
   let ulPagination = document.createElement('ul');
 
+  /* Calculation of the totalPages to be display in the bottom of the page. */
   let totalPages = Math.ceil(list.length / itemsToBeDisplayed);
-
+  /* Each pagination element is created adding a href and text contet */
   for (let i = 1; i <= totalPages; i++){
    let liPagination = document.createElement('li');
    let aPagination = document.createElement('a');
@@ -48,10 +51,12 @@ const createPaginationLinks = (list) => {
    liPagination.appendChild(aPagination);
    ulPagination.appendChild(liPagination);
 
+   /* By default, when the browser is refreshed, the page number 1 is highlighted */
    if(i === 1){
       aPagination.className = 'active';
    };
-
+   /* Event Listener is added to each li element that belongs to the pagination
+   When the users clicks one number, this is highlighted using active class.*/
    liPagination.addEventListener('click', (e) => {
       const aElements = document.querySelectorAll('a');
       const eventTarget = e.target;
@@ -72,21 +77,11 @@ const createPaginationLinks = (list) => {
 displayPage(studentList, 1);
 createPaginationLinks(studentList);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+/*
+In this section, all the HTML elements required to perform the search are created
+*/
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+const mainPage = document.querySelector('.page');
 const divHeader = document.querySelector('.page-header');
 const divSearch = document.createElement('div');
 divSearch.className = 'student-search';
@@ -98,13 +93,24 @@ divSearch.appendChild(inputSearch);
 divSearch.appendChild(buttonSearch);
 divHeader.appendChild(divSearch);
 
+/*
+Creation of the HTML elements to show "no results" message when the .includes()
+method is false
+*/
+const noResultsDiv = document.createElement('div');
+const titleMessage = document.createElement('h2');
+titleMessage.textContent = "Your search has not results";
+noResultsDiv.appendChild(titleMessage);
+
+/* Function responsible of performing the search. Requires the input value
+entered by the user and the list of studentes(users) to be searched. */
 const searchPerform = (searchInput, names) =>{
    let counter = 0;
    let filteredNames = [];
-   console.log(searchInput);
-   console.log(names);
    for(let i = 0; i < names.length; i++){
       if(searchInput.value.length !== 0){
+         /* Here is a comparison between lowercase strings. This method allows
+         comparisons when the users use uppercase letters */
          if(names[i].textContent.toLowerCase()
          .includes(searchInput.value.toLowerCase())){
             studentList[i].style.display = 'block';
@@ -119,22 +125,29 @@ const searchPerform = (searchInput, names) =>{
          filteredNames.push(studentList[i]);
      } 
 
-     }
+   }
+   /* Here, the current pagination is removed, because a new one 
+   will be generated*/
+   mainPage.removeChild(document.querySelector('.pagination'));
+   displayPage(filteredNames, 1);
    createPaginationLinks(filteredNames);
-}
 
+   /* A counter is added above to compare if the search has results or not */
+   if (counter === studentList.length) {
+      mainPage.insertBefore(noResultsDiv, mainPage.lastElementChild);
+      document.querySelector('.pagination').style.display = 'none';
+      noResultsDiv.style.display = 'block';
+   } else {
+      document.querySelector('.pagination').style.display = 'block';
+      noResultsDiv.style.display = 'none';  
+   }
+
+}
+/* Adding two different listener events to trigger the same function  */
 buttonSearch.addEventListener('click', () => {
    searchPerform(inputSearch, studentList);
 });
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+inputSearch.addEventListener('keyup', () => {
+   searchPerform(inputSearch, studentList);
+});
